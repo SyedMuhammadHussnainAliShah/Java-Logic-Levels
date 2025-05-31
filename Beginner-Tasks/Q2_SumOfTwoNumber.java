2357c8032430c689e5d04b4e22ea1ab07d3006cc
SYED MUHAMMAD HUSSNAIN ALI <syedhussnainshah313@gmail.com>
2025-05-31 17:17:04 +0500

    Sum Of Two Numbers

---
+++ Beginner-Tasks/Q2_SumOfTwoNumber.java
@@ -0,0 +1,8 @@
+public class Q2_SumOfTwoNumber {
+    public static void main(String[] args) {
+        int a=5;
+        int b=100;
+        int c=a+b;
+        System.out.println("the sum of two number is "+c);
+    }
+}
--- Intermediate-Tasks/Q1_FindTheLargestOfThree.java
+++ Intermediate-Tasks/Q1_FindTheLargestOfThree.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class LargestOfThree {
+public class Q1_FindTheLargestOfThree {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
